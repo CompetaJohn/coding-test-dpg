b277595eca63a48ba0165673d6e3fb70f990e095
CompetaJohn <80247543+CompetaJohn@users.noreply.github.com>
2022-06-16 09:26:40 +0200

    Begin with tidy-up: Further simplifications & more boolean checks added

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -21,17 +21,33 @@ export class GildedRose {
     return name === 'Sulfuras, Hand of Ragnaros'
   }
 
+  isBackstagePass(name: string):boolean {
+    return name === 'Backstage passes to a TAFKAL80ETC concert'
+  }
+
+  isAgedBrie(name: string):boolean {
+    return name === 'Aged Brie'
+  }
+
+  isBelowMaxQuality(quality: number): boolean {
+    return quality < 50
+  }
+
+  isAboveMinQuality(quality: number): boolean {
+    return quality > 0
+  }
+
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0 && !this.isLegendaryItem(this.items[i].name)) {
+      if (!this.isAgedBrie(this.items[i].name) && !this.isBackstagePass(this.items[i].name)) {
+        if (this.isAboveMinQuality(this.items[i].quality) && !this.isLegendaryItem(this.items[i].name)) {
             this.items[i].quality = this.items[i].quality - 1
         }
       } else {
-        if (this.items[i].quality < 50) {
+        if (this.isBelowMaxQuality(this.items[i].quality)) {
           this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality < 50) {
+          if (this.isBackstagePass(this.items[i].name)) {
+            if (this.isBelowMaxQuality(this.items[i].quality)) {
               if (this.items[i].sellIn < 11) {
                 this.items[i].quality = this.items[i].quality + 1
               }
@@ -46,18 +62,16 @@ export class GildedRose {
         this.items[i].sellIn = this.items[i].sellIn - 1;
       }
       if (this.items[i].sellIn < 0) {
-        if (this.items[i].name != 'Aged Brie') {
-          if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (!this.isLegendaryItem(this.items[i].name)) {
-                this.items[i].quality = this.items[i].quality - 1
-              }
+        if (!this.isAgedBrie(this.items[i].name)) {
+          if (!this.isBackstagePass(this.items[i].name)) {
+            if (this.isAboveMinQuality(this.items[i].quality) && !this.isLegendaryItem(this.items[i].name)) {
+              this.items[i].quality = this.items[i].quality - 1
             }
           } else {
             this.items[i].quality = this.items[i].quality - this.items[i].quality
           }
         } else {
-          if (this.items[i].quality < 50) {
+          if (this.isBelowMaxQuality(this.items[i].quality)) {
             this.items[i].quality = this.items[i].quality + 1
           }
         }
